0c9324eed124cf22066abe0d8448efa3479e186d
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-08 13:41:51 -0400

    Initial Commit

---
+++ Work1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Work1
+//
+//  Created by Daniel Kwolek on 8/8/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
